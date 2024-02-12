ce2cbea9e6c15fd8d87494436f0d9f1f9d581b37
Ericlkm <gtprinceeric.el1@gmail.com>
2024-02-12 00:37:59 -0500

    saving final changes

--- index.js
+++ index.js
@@ -36,35 +36,35 @@ fetch(apiUrl2)
     data.list[4].main.temp = Math.round(data.list[4].main.temp - 273.15 +32)
 
  $("#weather-1").append(`
-        <h3>${fiveDforecast[0].dt_txt.slice(0,-8)}</h3><br>
+        <h3>${fiveDforecast[1].dt_txt.slice(0,-8)}</h3><br>
         <h4>Temp: ${ data.list[1].main.temp}°c</h4><br>
         <h4>Humidity: ${ data.list[1].main.humidity}%</h4><br>
         <h4>Wind speed: ${ data.list[1].wind.speed}mph</h4>
     `)
     
     $("#weather-2").append(`
-    <h3>${fiveDforecast[1].dt_txt.slice(0,-8)}</h3><br>
-    <h4>Temp: ${ data.list[1].main.temp}°c</h4><br>
-    <h4>Humidity: ${ data.list[1].main.humidity}%</h4><br>
-    <h4>Wind speed: ${ data.list[1].wind.speed}mph</h4>
+    <h3>${fiveDforecast[2].dt_txt.slice(0,-8)}</h3><br>
+    <h4>Temp: ${ data.list[2].main.temp}°c</h4><br>
+    <h4>Humidity: ${ data.list[2].main.humidity}%</h4><br>
+    <h4>Wind speed: ${ data.list[2].wind.speed}mph</h4>
 `)
 $("#weather-3").append(`
-<h3>${fiveDforecast[2].dt_txt.slice(0,-8)}</h3><br>
-<h4>Temp: ${ data.list[2].main.temp}°c</h4><br>
-<h4>Humidity: ${ data.list[2].main.humidity}%</h4><br>
-<h4>Wind speed: ${ data.list[2].wind.speed}mph</h4>
-`)
-$("#weather-4").append(`
 <h3>${fiveDforecast[3].dt_txt.slice(0,-8)}</h3><br>
 <h4>Temp: ${ data.list[3].main.temp}°c</h4><br>
 <h4>Humidity: ${ data.list[3].main.humidity}%</h4><br>
 <h4>Wind speed: ${ data.list[3].wind.speed}mph</h4>
 `)
-$("#weather-5").append(`
+$("#weather-4").append(`
 <h3>${fiveDforecast[4].dt_txt.slice(0,-8)}</h3><br>
 <h4>Temp: ${ data.list[4].main.temp}°c</h4><br>
 <h4>Humidity: ${ data.list[4].main.humidity}%</h4><br>
 <h4>Wind speed: ${ data.list[4].wind.speed}mph</h4>
+`)
+$("#weather-5").append(`
+<h3>${fiveDforecast[5].dt_txt.slice(0,-8)}</h3><br>
+<h4>Temp: ${ data.list[5].main.temp}°c</h4><br>
+<h4>Humidity: ${ data.list[5].main.humidity}%</h4><br>
+<h4>Wind speed: ${ data.list[5].wind.speed}mph</h4>
 `)
 })
 }
